5620580162d9b8365013a1fdf0c7939a92b9e987
hyeonjae <>
2023-08-28 21:27:45 +0900

    최종수정본

--- app.js
+++ app.js
@@ -1,32 +1,36 @@
 import express from "express";
 import connect from "./schemas/index.js";
-import swaggerUi from 'swagger-ui-express'
-import swaggerJSDoc from 'swagger-jsdoc'
-import { options } from './swagger/config.js';
-import { postsRouter, commentsRouter } from './routes/index.js';
-
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+import { options } from "./swagger/config.js";
+import { postsRouter, commentsRouter } from "./routes/index.js";
 
 const app = express();
 const port = 6010;
 connect();
 
+// JSON 미들웨어 사용 설정
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(options)));
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// Swagger 문서 설정
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(options)));
+// 기본 경로 설정
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
+// 라우터 설정
 app.use("/posts", postsRouter);
 app.use("/posts", commentsRouter);
+
+// 오류 핸들링 미들웨어
 app.use((err, req, res, next) => {
-  console.error(err.message);
-  res.status(err.status || 500).send({ message: err.message }); 
+  console.error(err.stack); // 에러 스택 출력
+  res.status(err.status || 500).send({
+    status: err.status || 500,
+    message: err.message || "Internal Server Error",
+  });
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-
-
-
--- schemas/index.js
+++ schemas/index.js
@@ -2,8 +2,10 @@
 import mongoose from 'mongoose';
 import dotenv from "dotenv";
 
+// 환경 변수 설정
 dotenv.config();
 
+// 몽고디비 연결 
 const connect = () => {
   mongoose
     .connect(
@@ -12,10 +14,11 @@ const connect = () => {
         dbName: 'nodejs_lv1',
       },
     )
-    .catch((err) => console.log(err))
-    .then(() => console.log('몽고디비 연결 성공'));
+    .catch((err) => console.log(err)) // 연결 오류 핸들링
+    .then(() => console.log('몽고디비 연결 성공')); // 연결 성공 메시지
 };
 
+// 몽고디비 연결 에러 이벤트 리스너
 mongoose.connection.on('error', (err) => {
   console.error('몽고디비 연결 에러', err);
 });
